24b97c1749e8d741cc6c38233ce9a76386f43f80
REENkathure <102510435+REENkathure@users.noreply.github.com>
2022-04-01 13:16:37 +0300

    Add files via upload

---
+++ product of two numbers from the user.c
@@ -0,0 +1,20 @@
+//program to get the product of two numbers from the user
+#include<stdio.h>
+int main()
+{
+int m;
+int k;
+int product;
+printf("Enter the first number" );
+scanf("%d",&m);
+printf("Enter the second number" );
+scanf("%d",&k);
+product=m*k;
+printf("the product of m and y is %d", product);
+return 0;
+}
+
+
+
+   
+   
---
+++ program to print my name.c
@@ -0,0 +1,13 @@
+//program to print my name
+#include<stdio.h>
+int main()
+{printf("kathure Moreen");
+return 0;
+}
+
+
+
+
+
+   
+   
